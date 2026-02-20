3bf89a91ae2f2df99556ee0493cbe02f8ce28e2f
Aakash Srivastava <75659197+techqrt@users.noreply.github.com>
2026-02-20 05:08:26 -0500

    Update settings.py

--- media_converter/settings.py
+++ media_converter/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
